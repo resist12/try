1981beb6c30cd0591a5096a416cf9b1ca2a142b5
resist12 <88823387+resist12@users.noreply.github.com>
2021-11-01 17:47:47 +0100

    Create try.py

---
+++ try.py
@@ -0,0 +1,4 @@
+import os
+        os.system(f'git clone https://github.com/Vairous7x/V7x-Fishing2')
+        os.system(f'pip3 install -r requirements.txt')
+        os.system(f'sudo python3 start.py')
